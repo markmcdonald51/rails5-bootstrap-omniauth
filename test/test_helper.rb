704c353006fecfd784947649ca53c087b64e8820
Adam Lutz <amlutz160@gmail.com>
2016-04-10 13:59:18 -0500

    removing unnecceary reporters gem in rails5

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-# require 'minitest/reporters'
 require 'mocha/mini_test'
 require 'simplecov'
 
 SimpleCov.start
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true), Minitest::Reporters::ProgressReporter.new]
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
