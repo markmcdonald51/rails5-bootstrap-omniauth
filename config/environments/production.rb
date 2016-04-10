ada037c1d9043a02f72c2a6635d0ae09724eb5d7
Adam Lutz <amlutz160@gmail.com>
2016-04-10 15:29:14 -0500

    adding dynamic assets_host var for prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.assets.prefix = "/assets"
+  config.asset_host = ENV['ASSET_HOST']
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
